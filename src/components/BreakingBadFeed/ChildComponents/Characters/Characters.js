8df4ec151ba240ff11394cf9668344f2d03ef4d3
sauravApollo <saurav@apollofinvest.com>
2020-11-10 02:45:25 +0530

    Major: Implemented pagination in episodes and characters view

--- src/components/BreakingBadFeed/ChildComponents/Characters/Characters.js
+++ src/components/BreakingBadFeed/ChildComponents/Characters/Characters.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
+import Pagination from "react-js-pagination";
 import { CharacterCard } from "./components";
 
 const Characters = (props) => {
   const { data } = props;
+
+  const [activePage, setActivePage] = useState(1);
+  const [paginatedData, setPaginatedData] = useState(
+    data.slice(0, data.length > 20 ? 20 : data.length)
+  );
+
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+    setPaginatedData(
+      data.slice(
+        pageNumber === 1 ? 0 : (pageNumber - 1) * 20,
+        data.length < (pageNumber - 1) * 20 + 20
+          ? data.length
+          : (pageNumber - 1) * 20 + 20
+      )
+    );
+  };
+
   return (
-    <div className="d-flex flex-wrap">
-      {data.map((character) => (
-        <CharacterCard character={character} key={character.char_id} />
-      ))}
-    </div>
+    <Fragment>
+      <div className="d-flex flex-wrap mb-4">
+        {data.length > 20 ? paginatedData.map((character) => (
+          <CharacterCard character={character} key={character.char_id} />
+        )) : (
+          data.map((character) => (
+            <CharacterCard character={character} key={character.char_id} />
+          ))
+        )}
+      </div>
+      {
+        data.length > 20 && (
+          <div className="col-md-12">
+            <Pagination
+              activePage={activePage}
+              itemsCountPerPage={20}
+              totalItemsCount={data.length}
+              pageRangeDisplayed={5}
+              onChange={handlePageChange}
+              itemClass="page-item"
+              linkClass="page-link"
+            />
+          </div>
+        )
+      }
+    </Fragment>
   );
 };
 
--- src/components/BreakingBadFeed/ChildComponents/Episodes/Episodes.js
+++ src/components/BreakingBadFeed/ChildComponents/Episodes/Episodes.js
@@ -1,18 +1,47 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
+import Pagination from "react-js-pagination";
 import { Episode } from "./components";
 
 const Episodes = (props) => {
   const { allEpisodes, allCharacters } = props;
 
+  const [activePage, setActivePage] = useState(1);
+  const [paginatedData, setPaginatedData] = useState(
+    allEpisodes.slice(0, allEpisodes.length > 10 ? 10 : allEpisodes.length)
+  );
+
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+    setPaginatedData(
+      allEpisodes.slice(
+        pageNumber === 1 ? 0 : (pageNumber - 1) * 10,
+        allEpisodes.length < (pageNumber - 1) * 10 + 10
+          ? allEpisodes.length
+          : (pageNumber - 1) * 10 + 10
+      )
+    );
+  };
+
   return (
     <Fragment>
-      {allEpisodes.map((episode) => (
+      {paginatedData.map((episode) => (
         <Episode
           episode={episode}
           key={episode.episode_id}
           allCharacters={allCharacters}
         />
       ))}
+      <div className="col-md-12 mt-4">
+        <Pagination
+          activePage={activePage}
+          itemsCountPerPage={10}
+          totalItemsCount={allEpisodes.length}
+          pageRangeDisplayed={5}
+          onChange={handlePageChange}
+          itemClass="page-item"
+          linkClass="page-link"
+        />
+      </div>
     </Fragment>
   );
 };
