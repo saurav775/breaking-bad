8792153425ec57ea730e3799fab0963ba876a0de
sauravApollo <saurav@apollofinvest.com>
2020-11-10 01:13:37 +0530

    Minor: Episode wise character view implementation

--- src/components/BreakingBadFeed/ChildComponents/Episodes/Episodes.js
+++ src/components/BreakingBadFeed/ChildComponents/Episodes/Episodes.js
@@ -2,12 +2,16 @@ import React, { Fragment } from "react";
 import { Episode } from "./components";
 
 const Episodes = (props) => {
-  const { allEpisodes } = props;
+  const { allEpisodes, allCharacters } = props;
 
   return (
     <Fragment>
       {allEpisodes.map((episode) => (
-        <Episode episode={episode} key={episode.episode_id} />
+        <Episode
+          episode={episode}
+          key={episode.episode_id}
+          allCharacters={allCharacters}
+        />
       ))}
     </Fragment>
   );
--- src/components/BreakingBadFeed/ChildComponents/Episodes/components/Episode/Episode.js
+++ src/components/BreakingBadFeed/ChildComponents/Episodes/components/Episode/Episode.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Characters from "../../../Characters";
+import "./Episode.css";
 
 const Episode = (props) => {
-  const { episode } = props;
+  const {
+    episode: { episode, characters, season, title, air_date },
+    allCharacters,
+  } = props;
 
-  return <div>Episode</div>;
+  const [episodeCharacters, setEpisodeCharacters] = useState([]);
+
+  // filtering characters as per episode
+  useEffect(() => {
+    const chars = [];
+    allCharacters.forEach((element) => {
+      if (characters.includes(element.name)) {
+        chars.push(element);
+      }
+    });
+    setEpisodeCharacters(chars);
+  }, [episodeCharacters.length]);
+
+  return (
+    <div className="mt-5">
+      <div className="col-md-12">
+        <div className="episode-card text-light p-3 d-flex justify-content-between">
+          <div className="">
+            <span className="bold-text mr-2">Season:</span> {season}
+          </div>
+          <div className="">
+            <span className="bold-text mr-2">Episode:</span> {episode}
+          </div>
+          <div className="">
+            <span className="bold-text mr-2">Title:</span> {title}
+          </div>
+          <div className="">
+            <span className="bold-text mr-2">On Air:</span> {air_date}
+          </div>
+        </div>
+      </div>
+      <Characters data={episodeCharacters} />
+    </div>
+  );
 };
 
 export default Episode;
